789342e15b3b9a21c2bc7c6a946acc8ae542b8bf
jhnmss-source <jhnmssslcd@gmail.com>
2026-04-23 09:33:21 +0800

    Delete job_system directory

--- job_system/editdev.php
+++
@@ -1,9 +0,0 @@
-<?php require_once 'core/models.php'; ?>
-<?php require_once 'core/dbConfig.php'; ?>
-<h1>Edit Developer</h1>
-<form action="core/handleForms.php?dev_id=<?php echo $_GET['dev_id']; ?>" method="POST">
-    <p><label>First Name</label> <input type="text" name="first_name"></p>
-    <p><label>Last Name</label> <input type="text" name="last_name"></p>
-    <p><label>Specialization</label> <input type="text" name="specialization"></p>
-    <input type="submit" name="editDevBtn">
-</form>
